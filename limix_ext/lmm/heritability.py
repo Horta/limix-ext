5166bc7771c37d5e2277acd19e0c752a5b125a5c
Danilo Horta <danilo.horta@gmail.com>
2016-03-30 17:04:35 +0100

    lmm binomial h2

--- limix_ext/lmm/heritability.py
+++ limix_ext/lmm/heritability.py
@@ -1,12 +1,48 @@
+from __future__ import division
 import numpy as np
 from numpy import asarray
 from limix_tool.heritability import h2_observed_space_correct
 from limix_tool.kinship import gower_kinship_normalization
 import core
 
-def estimate(y, covariate, K, prevalence):
+def estimate(y, covariate, K, prevalence, ntrials=None):
     y = asarray(y, float).copy()
-    return _bernoulli_estimator(y, covariate, K, prevalence)
+    if ntrials is None:
+        return _bernoulli_estimator(y, covariate, K, prevalence)
+    return _binomial_estimator(y, covariate, K, ntrials, prevalence)
+
+def _binomial_estimator(y, covariate, K, ntrials, prevalence):
+    y = y.copy() / ntrials
+    covariate = covariate.copy()
+    y -= y.mean()
+    std = y.std()
+    if std > 0.:
+        y /= std
+    K = gower_kinship_normalization(asarray(K, float))
+
+    n = K.shape[0]
+    G = np.random.randint(0, 3, size=(n, 1))
+    G = np.asarray(G, float)
+    y_ = y[:, np.newaxis]
+
+    (_, _, ldelta, sigg2, beta0) = core.train_associations(G, y_,
+                                                  K, C=covariate,
+                                                  addBiasTerm=False)
+
+    m = np.dot(covariate, beta0.T)
+    varc = np.var(m)
+
+    delta = np.exp(ldelta)
+    sige2 = delta * sigg2
+    h2 = sigg2 / (sigg2 + sige2 + varc)
+    h2 = np.asscalar(np.asarray(h2, float))
+
+    h2 = h2_observed_space_correct(h2, prevalence, prevalence)
+
+    if not np.isfinite(h2):
+        h2 = 0.
+    return h2
+
 
 def _bernoulli_estimator(y, covariate, K, prevalence):
     y = y.copy()
@@ -37,4 +73,6 @@ def _bernoulli_estimator(y, covariate, K, prevalence):
 
     h2 = h2_observed_space_correct(h2, prevalence, ascertainment)
 
+    if not np.isfinite(h2):
+        h2 = 0.
     return h2
