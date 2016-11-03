5a30bd1e8ba7af30cb386e37a8ddf951e26c2263
Danilo Horta <danilo.horta@gmail.com>
2016-11-03 13:53:36 +0000

    qtl normal scan

--- limix_ext/lmm/qtl.py
+++ limix_ext/lmm/qtl.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import
 
 import logging
 
-import numpy as np
-from numpy import asarray
+from numpy import ascontiguousarray
+from numpy import logical_not
+from numpy import isfinite
+from numpy import newaxis
 
-from limix_tool.kinship import gower_kinship_normalization
+from ..util import gower_normalization
+from ..util import clone
 
 from ._core import train_associations
 
@@ -13,7 +16,7 @@ from ._core import train_associations
 def normal_scan(y, covariate, X, K, ntrials=None):
     logger = logging.getLogger(__name__)
     logger.info('Gower normalizing')
-    K = gower_kinship_normalization(asarray(K, float))
+    K = gower_normalization(asarray(K, float))
     X = asarray(X, float).copy()
     y = asarray(y, float).copy()
     covariate = asarray(covariate, float).copy()
@@ -26,7 +29,7 @@ def normal_scan(y, covariate, X, K, ntrials=None):
     if std > 0.:
         y /= std
 
-    y = y[:, np.newaxis]
+    y = y[:, newaxis]
 
 
     logger.info('train_association started')
@@ -41,3 +44,37 @@ def normal_scan(y, covariate, X, K, ntrials=None):
     stats[nok] = 0.
 
     return (stats, pvals)
+
+def binomial_scan(nsuccesses, ntrials, X, K, covariates):
+    logger = logging.getLogger(__name__)
+    logger.info('Gower normalizing')
+
+    nsuccesses = clone(nsuccesses)
+    ntrials = clone(ntrials)
+    X = clone(X)
+    K = clone(K)
+    covariates = clone(covariates)
+
+    gower_normalization(K, out=K)
+
+    nsuccesses /= ntrials
+
+    nsuccesses -= nsuccesses.mean()
+    std = nsuccesses.std()
+    if std > 0.:
+        nsuccesses /= std
+
+    nsuccesses = nsuccesses[:, newaxis]
+
+    logger.info('train_association started')
+    (stats, pvals, _, _, _) = train_associations(X, nsuccesses, K, C=covariates,
+                                                 addBiasTerm=False)
+    logger.info('train_association finished')
+    pvals = ascontiguousarray(pvals, float).ravel()
+    nok = logical_not(isfinite(pvals))
+    pvals[nok] = 1.
+
+    stats = ascontiguousarray(stats, float).ravel()
+    stats[nok] = 0.
+
+    return pvals
