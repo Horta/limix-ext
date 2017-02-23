0fd14b4271ddedde49769c9c1075d718889f74a4
Danilo Horta <danilo.horta@gmail.com>
2017-02-23 11:36:51 +0000

    scipy_sugar

--- limix_ext/lmm/heritability.py
+++ limix_ext/lmm/heritability.py
@@ -3,20 +3,20 @@ from __future__ import division
 import numpy as np
 import scipy as sp
 import scipy.stats
-from numpy import asarray
+from numpy import asarray, newaxis
 
-from ._core import train_associations
+from scipy_sugar.stats import quantile_gaussianize
+
 from ..util import gower_normalization
+from ._core import train_associations
+
 
 def binomial_estimate(nsuccesses, ntrials, covariate, K):
     ntrials = np.asarray(ntrials, float)
     nsuccesses = np.asarray(nsuccesses, float).copy()
     ntrials = np.asarray(ntrials, float).copy()
 
-    if rank_normalize:
-        phenotype = quantile_gaussianize(nsuccesses / ntrials)
-    else:
-        phenotype = 1 - norm.isf(clip(1 - nsuccesses / ntrials, 1e-10, 1-1e-10))
+    phenotype = quantile_gaussianize(nsuccesses / ntrials)
 
     phenotype = phenotype[:, newaxis]
 
@@ -29,9 +29,8 @@ def binomial_estimate(nsuccesses, ntrials, covariate, K):
     G = np.random.randint(0, 3, size=(n, 1))
     G = np.asarray(G, float)
 
-    (_, _, ldelta, sigg2, beta0) = train_associations(G, phenotype,
-                                                  K, C=covariate,
-                                                  addBiasTerm=False)
+    (_, _, ldelta, sigg2, beta0) = train_associations(
+        G, phenotype, K, C=covariate, addBiasTerm=False)
 
     m = np.dot(covariate, beta0.T)
     varc = np.var(m)
@@ -44,6 +43,7 @@ def binomial_estimate(nsuccesses, ntrials, covariate, K):
     if not np.isfinite(h2):
         h2 = 0.
     return h2
+
 
 def poisson_estimate(y, covariate, K):
     ntrials = np.asarray(ntrials, float)
@@ -62,9 +62,8 @@ def poisson_estimate(y, covariate, K):
     G = np.asarray(G, float)
     y_ = y[:, np.newaxis]
 
-    (_, _, ldelta, sigg2, beta0) = train_associations(G, y_,
-                                                  K, C=covariate,
-                                                  addBiasTerm=False)
+    (_, _, ldelta, sigg2, beta0) = train_associations(
+        G, y_, K, C=covariate, addBiasTerm=False)
 
     m = np.dot(covariate, beta0.T)
     varc = np.var(m)
@@ -82,7 +81,7 @@ def poisson_estimate(y, covariate, K):
 def _bernoulli_estimator(y, covariate, K, prevalence, **kwargs):
     y = y.copy()
     covariate = covariate.copy()
-    ascertainment = float(sum(y==1.)) / len(y)
+    ascertainment = float(sum(y == 1.)) / len(y)
     y -= y.mean()
     std = y.std()
     if std > 0.:
@@ -94,9 +93,8 @@ def _bernoulli_estimator(y, covariate, K, prevalence, **kwargs):
     G = np.asarray(G, float)
     y_ = y[:, np.newaxis]
 
-    (_, _, ldelta, sigg2, beta0) = train_associations(G, y_,
-                                                  K, C=covariate,
-                                                  addBiasTerm=False)
+    (_, _, ldelta, sigg2, beta0) = train_associations(
+        G, y_, K, C=covariate, addBiasTerm=False)
 
     m = np.dot(covariate, beta0.T)
     varc = np.var(m)
