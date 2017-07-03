0293b1a6ac758816a8c72008c34577120391c068
Danilo Horta <danilo.horta@gmail.com>
2017-07-03 17:05:37 +0100

    leap her

--- limix_ext/util.py
+++ limix_ext/util.py
@@ -27,31 +27,6 @@ def clone(X):
 
 
 def maf(X):
-    r"""Compute minor allele frequencies.
-
-    It assumes that `X` encodes 0, 1, and 2 representing the number
-    of alleles.
-
-    Args:
-        X (array_like): Genotype matrix.
-
-    Returns:
-        array_like: minor allele frequencies.
-
-    Examples
-    --------
-
-        .. doctest::
-
-            >>> from numpy.random import RandomState
-            >>> from limix.stats import maf
-            >>>
-            >>> random = RandomState(0)
-            >>> X = random.randint(0, 3, size=(100, 10))
-            >>>
-            >>> print(maf(X))
-            [ 0.49   0.49   0.445  0.495  0.5    0.45   0.48   0.48   0.47   0.435]
-    """
     ok = _check_encoding(X)
     if not ok:
         raise ValueError("It assumes that X encodes 0, 1, and 2 only.")
